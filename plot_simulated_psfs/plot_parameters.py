45f15089ef6fd5a231ba80964e64caabf69abbf7
Matthew-Freeman <matthew.s.r.freeman@gmail.com>
2020-09-21 12:03:02 -0700

    Upload python scripts to plot_simulated_psfs/

---
+++ plot_simulated_psfs/plot_parameters.py
@@ -0,0 +1,199 @@
+from astropy.io import fits
+from astropy.modeling import models, fitting
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import os
+
+
+#This python script takes the 4D fits files produced by reshape_fits.py, calculates the parameters of the psf, such as 
+#FWHM and EE50, and plots the results as 2D grids.
+#By Matthew Freeman.
+
+input_directory = './fits_output/'
+output_directory = './plots/'
+fit_threshold = 0.05   	#all data below this fraction of the max will be masked out for the gaussian fit.
+
+def main():
+	if not os.path.exists(output_directory):
+		os.makedirs(output_directory)
+
+	for fits_filename in os.listdir(input_directory):
+		if fits_filename[-4:] == 'fits':
+			process_fits(input_directory,fits_filename)
+
+def process_fits(directory, FITSfilename):
+
+	with fits.open(directory + FITSfilename) as psfFITS:
+		header = psfFITS[0].header
+		data = psfFITS[0].data
+		strehl_data = psfFITS[1].data
+
+	print("Processing", FITSfilename)
+
+	sim_parameters = {'label': FITSfilename[:-16],
+						'psf_spacing' : header['PSFSPACE'],
+						'pixel_scale' : header['PIXSCALE'],
+						'TT_offset' : [header['NGSX'],header['NGSY']],
+						'laser_radius' : header['LGSRAD'],
+						'psf_size' : data.shape[2],
+						'grid_size' : data.shape[0],
+						}
+	
+	psf_size = sim_parameters['psf_size']
+	grid_size = sim_parameters['grid_size']
+	psf_parameters = np.zeros((grid_size,grid_size,4)) #x_fwhm, y_fwhm, theta, 50% encircled energy
+	y_coords, x_coords = np.mgrid[:psf_size,:psf_size]
+	fit = fitting.LevMarLSQFitter()
+	data_masked = np.ma.empty((grid_size,grid_size,psf_size,psf_size))  	#mask out data in the wings
+	mask_level = np.zeros((grid_size,grid_size))
+
+	for i in range(grid_size):
+		for j in range(grid_size):
+			mask_level[i,j] = np.max(data[i,j,:,:])*fit_threshold
+			data_masked[i,j,:,:] = np.ma.masked_less(data[i,j,:,:],mask_level[i,j])
+			input_params = initial_gauss_params(data[i,j,:,:])
+			gauss_init = models.Gaussian2D(*input_params)
+			fitted_gauss = fit(gauss_init,x_coords,y_coords,data_masked[i,j,:,:])
+			psf_parameters[i,j,0] = fitted_gauss.x_fwhm
+			psf_parameters[i,j,1] = fitted_gauss.y_fwhm
+			psf_parameters[i,j,2] = fitted_gauss.theta.value
+			psf_parameters[i,j,3] = encircle_energy(data[i,j,:,:],fitted_gauss.y_mean.value,fitted_gauss.x_mean.value)
+			# plot_slice(data,i,j,fitted_gauss,mask_level,output_directory)
+
+	fwhm_grid = (psf_parameters[:,:,0] + psf_parameters[:,:,1] )/2.0 * sim_parameters['pixel_scale']
+	encircled_grid = psf_parameters[:,:,3] * sim_parameters['pixel_scale']
+	datasquare = np.zeros((psf_size*grid_size,psf_size*grid_size))  #reshape the 4D cube into a 2D grid
+	for i in range(grid_size):
+		for j in range (grid_size):
+			datasquare[i*psf_size:(i+1)*psf_size,j*psf_size:(j+1)*psf_size] = data[i,j,:,:]
+
+	plt.close('all')
+	plot_psfs(datasquare, output_directory, sim_parameters)
+	plot_fwhm(fwhm_grid, output_directory, sim_parameters)
+	plot_ee50(encircled_grid, output_directory, sim_parameters)
+	plot_strehl(strehl_data, output_directory, sim_parameters)
+
+
+def initial_gauss_params(data):  #takes a psf, calculates initial parameters for the gaussian fit.
+   total = data.sum()
+   y1, x1 = np.indices(data.shape)  #are the order of x and y correct here?
+   x2 = (x1 * data).sum() / total
+   y2 = (y1 * data).sum() / total
+   col = data[:, int(y2)]
+   row = data[int(x2), :]
+   sigmax = np.sqrt(abs(((np.arange(col.size) - y2) ** 2) * col).sum() / col.sum())
+   sigmay = np.sqrt(abs(((np.arange(row.size) - x2) ** 2) * row).sum() / row.sum())
+   height = np.ptp(data)
+   return [height, x2, y2, sigmax, sigmay]
+
+def encircle_energy(data,ymean,xmean):
+	data = data / data.sum()
+	Y, X = np.indices(data.shape)
+	distance = np.sqrt((Y-ymean)**2 + (X-xmean)**2)
+	trials = 201
+	circles = np.zeros((trials,2))
+	circles[:,0] = np.linspace(0,20,trials)   #[radius, encircled value]
+	for n, r in enumerate(circles[:,0]):
+		x_index, y_index = np.where(distance <= r)
+		circles[n,1] = np.sum(data[x_index, y_index])
+	ee50 = circles[np.argmin(np.abs(circles[:,1]-0.5)),0]
+	return ee50
+
+def plot_psfs(data_grid,output_directory,sim_parameters):
+	grid_size = sim_parameters['grid_size']
+	psf_spacing = sim_parameters['psf_spacing']
+	laser_radius = sim_parameters['laser_radius']
+	plt.figure(0)
+	plt.imshow(data_grid, norm=LogNorm(vmin=np.min(data_grid), vmax=1E10),origin='lower', extent = (grid_size, -grid_size, -grid_size, grid_size))
+	plt.plot([-laser_radius,0,0,laser_radius],[0,-laser_radius,laser_radius,0],'o',color='magenta')
+	plt.plot(sim_parameters['TT_offset'][0],sim_parameters['TT_offset'][1],'or')
+	plt.xlabel("East (arcseconds)")
+	plt.ylabel("North (arcseconds)")
+	tickpoints = np.linspace( -(grid_size-1)/2*psf_spacing , (grid_size-1)/2*psf_spacing , grid_size)
+	plt.xticks(tickpoints)	
+	plt.yticks(tickpoints)
+	ax = plt.gca()
+	plt.setp(ax.get_xticklabels()[0::2], visible=False)
+	plt.setp(ax.get_yticklabels()[0::2], visible=False)
+	cbar = plt.colorbar()
+	cbar.set_label("Count", rotation=90)
+	plt.savefig(output_directory + sim_parameters['label'] + "_psfs.png", bbox_inches='tight', dpi=250)
+
+def plot_fwhm(fwhm_grid,output_directory,sim_parameters):
+	grid_size = sim_parameters['grid_size']
+	psf_spacing = sim_parameters['psf_spacing']
+	laser_radius = sim_parameters['laser_radius']
+	plt.figure(1)
+	plt.imshow(fwhm_grid*1000, cmap='viridis_r', origin='lower', extent = (grid_size, -grid_size, -grid_size, grid_size))
+	plt.plot([-laser_radius,0,0,laser_radius],[0,-laser_radius,laser_radius,0],'o',color='magenta')
+	plt.plot(sim_parameters['TT_offset'][0],sim_parameters['TT_offset'][1],'or')
+	plt.xlabel("East (arcseconds)")
+	plt.ylabel("North (arcseconds)")
+	tickpoints = np.linspace( -(grid_size-1)/2*psf_spacing , (grid_size-1)/2*psf_spacing , grid_size)
+	plt.xticks(tickpoints)	
+	plt.yticks(tickpoints)
+	ax = plt.gca()
+	plt.setp(ax.get_xticklabels()[0::2], visible=False)
+	plt.setp(ax.get_yticklabels()[0::2], visible=False)
+	cbar = plt.colorbar()
+	cbar.set_label("FWHM (milliarcseconds)", rotation=90)
+	plt.savefig(output_directory + sim_parameters['label'] + "_fwhm.png", bbox_inches='tight', dpi=250)
+
+def plot_ee50(ee_grid,output_directory,sim_parameters):
+	grid_size = sim_parameters['grid_size']
+	psf_spacing = sim_parameters['psf_spacing']
+	laser_radius = sim_parameters['laser_radius']
+	plt.figure(2)
+	plt.imshow(ee_grid*1000, cmap='viridis_r',origin='lower', extent = (grid_size, -grid_size, -grid_size, grid_size))
+	plt.plot([-laser_radius,0,0,laser_radius],[0,-laser_radius,laser_radius,0],'o',color='magenta')
+	plt.plot(sim_parameters['TT_offset'][0],sim_parameters['TT_offset'][1],'or')
+	plt.xlabel("East (arcseconds)")
+	plt.ylabel("North (arcseconds)")
+	tickpoints = np.linspace( -(grid_size-1)/2*psf_spacing , (grid_size-1)/2*psf_spacing , grid_size)
+	plt.xticks(tickpoints)	
+	plt.yticks(tickpoints)
+	ax = plt.gca()
+	plt.setp(ax.get_xticklabels()[0::2], visible=False)
+	plt.setp(ax.get_yticklabels()[0::2], visible=False)
+	cbar = plt.colorbar()
+	cbar.set_label("EE50 radius (milliarcseconds)", rotation=90)
+	plt.savefig(output_directory + sim_parameters['label'] + "_ee50.png", bbox_inches='tight', dpi=250)
+
+def plot_strehl(strehl_grid,output_directory,sim_parameters):
+	grid_size = sim_parameters['grid_size']
+	psf_spacing = sim_parameters['psf_spacing']
+	laser_radius = sim_parameters['laser_radius']
+	plt.figure(3)
+	plt.imshow(strehl_grid, cmap='viridis', origin='lower', extent = (grid_size, -grid_size, -grid_size, grid_size))
+	plt.plot([-laser_radius,0,0,laser_radius],[0,-laser_radius,laser_radius,0],'o',color='magenta')
+	plt.plot(sim_parameters['TT_offset'][0],sim_parameters['TT_offset'][1],'or')
+	plt.xlabel("East (arcseconds)")
+	plt.ylabel("North (arcseconds)")
+	tickpoints = np.linspace( -(grid_size-1)/2*psf_spacing , (grid_size-1)/2*psf_spacing , grid_size)
+	plt.xticks(tickpoints)	
+	plt.yticks(tickpoints)
+	ax = plt.gca()
+	plt.setp(ax.get_xticklabels()[0::2], visible=False)
+	plt.setp(ax.get_yticklabels()[0::2], visible=False)
+	cbar = plt.colorbar()
+	cbar.set_label("Strehl ratio (percentage)", rotation=90)
+	plt.savefig(output_directory + sim_parameters['label'] + "_strehl.png", bbox_inches='tight', dpi=250)
+
+def plot_slice(data,i,j,fitted_gauss,mask_level,output_directory,sim_parameters):
+	psf_size = sim_parameters['psf_size']
+	plt.figure(4)
+	plt.clf()
+	plt.plot([0,psf_size],[mask_level[i,j],mask_level[i,j]])
+	plt.plot(data[i,j,60,:],'.-',label="data")
+	plt.plot(np.linspace(50,70,501),fitted_gauss(np.linspace(50,70,501),60),label="fit")
+	plt.legend()
+	plt.xlim([50, 70])
+	plt.xticks([50,55,60,65,70])
+	plt.ylim([0, np.max(data)])
+	plt.savefig(output_directory + "gaussian_slice/" + str(10*j+i) + ".jpg", bbox_inches='tight')	
+
+main()
+
+
+
---
+++ plot_simulated_psfs/reshape_fits.py
@@ -0,0 +1,108 @@
+from astropy.io import fits
+import numpy as np
+
+# This python script takes in the strip of simulated psfs produced by Carlos and reformats them into a 4 dimensional array.
+# The dimensions are [y position of psf, x position of psf, y pixel in psf, x pixel in psf.]
+# By Matthew Freeman
+
+#------PARAMETERS------
+directories = ['./KAPA_PDR_PSFs/darkMatter/', 				#Location of each FITS file
+				'./KAPA_PDR_PSFs/galacticCenter/',
+				'./KAPA_PDR_PSFs/galaxyFormation/',
+				'KAPA_PDR_PSFs/gasGiantPlanets/']
+
+FITSfilenames = ['PSF2D_Hband_NyqSampl_LgsAstRadius7p6arcsec_ZA30_5000iter_4LGSsquare.fits',		#Name of each data FITS file
+				'PSF2D_Hband_NyqSampl_LgsAstRadius7p6arcsec_ZA50_5000iter_4LGSsquare.fits',
+				'PSF2D_Hband_NyqSampl_LgsAstRadius7p6arcsec_ZA30_5000iter_4LGSsquare.fits',
+				'PSF2D_Hband_NyqSampl_LgsAstRadius7p6arcsec_ZA15_5000iter_4LGSsquare.fits'
+				]
+
+strehlfilenames = ['SR_Hband_LgsAstRadius7p6arcsec_ZA30_5000iter_4LGSsquare.fits',			#Name of each strehl ratio FITS file
+				'SR_Hband_LgsAstRadius7p6arcsec_ZA50_5000iter_4LGSsquare.fits',
+				'SR_Hband_LgsAstRadius7p6arcsec_ZA30_5000iter_4LGSsquare.fits',
+				'SR_Hband_LgsAstRadius7p6arcsec_ZA15_5000iter_4LGSsquare.fits',
+				]
+
+labels = ["darkMatter",					#A label for the output FITS file
+			"galacticCenter",
+			"galaxyFormation",
+			"gasGiantPlanets"
+			]
+
+output_directory = './fits_output/'
+
+TT_offsets = [[-1.1172, 0.1056],			#x,y offset of primary tip tilt star in arcseconds. For plotting later.
+				[0,5.4],
+				[1.7008,7.853],
+				[-0.1109,0.1364]
+				]	
+
+pixel_scale = 10.0/1000 #arcseconds per pixel
+psf_spacing = 2.0   #spacing between generated psfs in arcseconds
+laser_radius = 7.6  #LGS asterism radius in arcseconds
+
+
+#------UNUSED PARAMETERS------
+wavelength = 1654 			#nanometers
+zenith_angles = [30,50,30,15]   #degrees
+
+def main():
+	if not os.path.exists(output_directory):
+		os.makedirs(output_directory)
+	for n in range(len(FITSfilenames)):
+		reshape(directories[n],FITSfilenames[n],strehlfilenames[n],labels[n],n)
+
+
+def reshape(directory, FITSfilename,strehlfilename,label,n):
+
+	with fits.open(directory + FITSfilename) as psfFITS:
+		header = psfFITS[0].header
+		data = psfFITS[0].data
+	with fits.open(directory + strehlfilename) as psfFITS:
+		strehl_header = psfFITS[0].header
+		strehl_data = psfFITS[0].data
+	# with fits.open(directory + "refPsf.fits") as refFITS:
+	# 	ref_header = refFITS[0].header
+	# 	ref_data = refFITS[0].data
+
+	print("Reshaping", FITSfilename)  #shape is (y,x). Fastest changing axis (x) is printed last
+	# print(repr(header))
+
+	psf_size = data.shape[0]
+	number_psfs = int(data.shape[1]/data.shape[0])
+	grid_size = int(np.sqrt(number_psfs))
+
+	data4D = np.zeros([grid_size,grid_size,psf_size,psf_size])
+	for i in range(grid_size):  #y value
+		for j in range(grid_size):   #x value
+			data4D[i,j,:,:] = data[:,(grid_size*j+i)*psf_size:(grid_size*j+i+1)*psf_size]
+
+	print(data.shape, '->', data4D.shape )
+
+	strehl_grid = np.zeros((grid_size,grid_size))	#reshape the strip of strehl data into a 2D grid
+	for i in range (grid_size):
+		for j in range(grid_size):
+			strehl_grid[i,j] = strehl_data[0,grid_size*j+i]
+
+	hdu_data = fits.PrimaryHDU(data4D)
+	hdu_strehl = fits.ImageHDU(strehl_grid, name='Strehl')
+	hdul = fits.HDUList([hdu_data,hdu_strehl])
+	hdul[0].header['PSFSPACE'] = (psf_spacing, 'psf spacing (arcseconds)')
+	hdul[0].header['PIXSCALE'] = (pixel_scale, 'pixel scale (arcseconds)')
+	hdul[0].header['NGSX'] = (TT_offsets[n][0], 'NGS x offset (arcseconds)')
+	hdul[0].header['NGSY'] = (TT_offsets[n][1], 'NGS x offset (arcseconds)')
+	hdul[0].header['LGSRAD'] = (laser_radius, 'LGS radius (arcseconds)')
+	hdul[0].header.comments['NAXIS1'] = 'pixel x'
+	hdul[0].header.comments['NAXIS2'] = 'pixel y'
+	hdul[0].header.comments['NAXIS3'] = 'psf x'
+	hdul[0].header.comments['NAXIS4'] = 'psf y'		
+	hdul[1].header.comments['NAXIS1'] = 'psf x'
+	hdul[1].header.comments['NAXIS2'] = 'psf y'
+
+	hdul.writeto(output_directory + label + '_simulation.fits',overwrite=True)
+
+
+main()
+
+
+
